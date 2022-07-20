cdca9467b95ee3c279d92b91b567061985ed9473
DavidHernandezMorones <david.2.hernandez@atos.net>
2022-07-20 17:01:03 -0500

    Clase hija de Pair

---
+++ src/MyCustomPair.cs
@@ -0,0 +1,11 @@
+﻿namespace Intermediate {
+	public class MyCustomPair : Pair {
+		public MyCustomPair(string key, dynamic value) {
+			Key = key;
+			Value = value;
+		}
+
+		public override string Key { get; set; }
+		public override dynamic Value { get; set; }
+	}
+}
