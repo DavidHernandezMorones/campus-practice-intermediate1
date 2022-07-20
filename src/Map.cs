837661fe09822ca9ce017118ed720b2fadb55b63
DavidHernandezMorones <david.2.hernandez@atos.net>
2022-07-20 17:01:21 -0500

    Clase Map con Generics

---
+++ src/Map.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Intermediate {
+	public class Map<T>
+	where T : Pair {
+		private T[] _pairs;
+		public Map(int max) {
+			_pairs = new T[max];
+		}
+
+		public void Put(int i, T t) {
+			_pairs[i] = t;
+		}
+
+		public T GetAt(int i) {
+			return _pairs[i];
+		}
+
+		public T Get(string key) {
+			foreach (T pair in _pairs) {
+				if (pair != null && pair.Key == key)
+					return pair;
+			}
+
+			throw new Exception("Key not found!");
+		}
+	}
+}
