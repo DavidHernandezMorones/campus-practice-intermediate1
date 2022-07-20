4eb2fa9140d78547dd302809c499f050ce0f9985
DavidHernandezMorones <david.2.hernandez@atos.net>
2022-07-20 16:34:26 -0500

    Extensions methods

---
+++ src/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Intermediate {
+	public static class Utils {
+		
+		public static void ToConsole(this object o, string msg) {
+			Console.WriteLine($"{msg}{o}");
+		}
+
+		public static bool CustomMatch(this string str) {
+			return Regex.IsMatch(str, @"^[A-Z][a-z]{3}\d{4}");
+		}
+		
+		public static T GetFirst<T>(this Map<T> map) where T : Pair {
+			return map.GetAt(0);
+		}
+
+	}
+}
